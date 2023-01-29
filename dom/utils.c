298b338fea2ab6f9129e57492e81587ea1144d93
iammky <iamkky@nowhere>
2023-01-29 17:54:34 -0500

    Added heTextf

--- dom/utils.c
+++ dom/utils.c
@@ -1,3 +1,5 @@
+#include "stdlib.h"
+#include "abd/printf.h"
 #include "helium/He.h"
 
 HeAttr heClass(char *class_name)
@@ -13,6 +15,26 @@ HeAttr heId(char *id_name)
 He heText(char *text)
 {
 	return heNew(HELIUM_TEXT_TAG, text);
+}
+
+He heTextf(char *fmt, ...)
+{
+va_list args;
+char	buffer[128], *buffer2;
+int	size;
+
+	va_start(args, fmt);
+
+	if((size = vsnprintf(buffer, 128, fmt, args)) < 128){
+		buffer2 = buffer;
+	}else{
+		if((buffer2 = malloc(size+1)) == NULL) return NULL;
+		vsnprintf(buffer2, size+1, fmt, args);
+	}
+
+	va_end(args);
+
+	return heNew(HELIUM_TEXT_TAG, buffer2);
 }
 
 He heList()
